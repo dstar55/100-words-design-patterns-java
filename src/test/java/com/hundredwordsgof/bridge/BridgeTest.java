3a16dd51312c789f246be33dc899c72e6343e800
dstar <dstar55@yahoo.com>
2017-02-25 22:50:27 +0100

    #25 indentation decreased from 4 to 2

--- src/test/java/com/hundredwordsgof/bridge/BridgeTest.java
+++ src/test/java/com/hundredwordsgof/bridge/BridgeTest.java
@@ -10,16 +10,20 @@ import org.junit.Test;
 public class BridgeTest {
 
 	@Test
-	public void testBuilder(){
+	public void testBuilder() {
 
 		// creates refined abstraction with concreteimplementorA
-		RefinedAbstraction refinedAbstractionA = new RefinedAbstraction(new ConcreteImplementorA());
-		//invokes operation
-		assertEquals("com.hundredwordsgof.bridge.ConcreteImplementorA", refinedAbstractionA.operation());
-		
+		RefinedAbstraction refinedAbstractionA = new RefinedAbstraction(
+				new ConcreteImplementorA());
+		// invokes operation
+		assertEquals("com.hundredwordsgof.bridge.ConcreteImplementorA",
+				refinedAbstractionA.operation());
+
 		// creates refined abstraction with concreteimplementorB
-		RefinedAbstraction refinedAbstractionB = new RefinedAbstraction(new ConcreteImplementorB());
-		//invokes operation
-		assertEquals("com.hundredwordsgof.bridge.ConcreteImplementorB", refinedAbstractionB.operation());		
-	}	
+		RefinedAbstraction refinedAbstractionB = new RefinedAbstraction(
+				new ConcreteImplementorB());
+		// invokes operation
+		assertEquals("com.hundredwordsgof.bridge.ConcreteImplementorB",
+				refinedAbstractionB.operation());
+	}
 }
