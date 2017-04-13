d7a51c8bbe7b704364e146f271aa6425d5385c81
dstar55 <dstar55@yahoo.com>
2017-04-13 10:44:55 +0200

    test_parser updated

--- test_parser.py
+++ test_parser.py
@@ -1,5 +1,5 @@
 '''
-Created on 14 Apr 2017
+Created on 12 Apr 2017
 
 @author: dstar55@yahoo.com
 '''
@@ -13,7 +13,7 @@ class TestParser(unittest.TestCase):
     def setup(self):
         pass
 
-    def test_parseReadmet(self):
+    def test_parseReadme(self):
         
         # parse method returns arrayList which is 
         arrayList = parser.parseReadme('./TestReadme.md')
@@ -22,17 +22,28 @@ class TestParser(unittest.TestCase):
         self.assertEqual(23, len(arrayList))
         
         # first member of the array is singleton dictionary
-        singleton = arrayList[0] 
+        singletonContent = arrayList[0] 
         
         # check Singleton
-        self.assertEqual("Singleton", singleton.get(constants.DICT_KEY_PATTERN_ID))        
-        self.assertEqual("Singleton", singleton.get(constants.DICT_KEY_PATTERN_NAME))
-        self.assertEqual("singleton.png", singleton.get(constants.DICT_KEY_PATTERN_UML_FILE_NAME))        
-        self.assertEqual(True, ("Singleton ensures that only one(single) object can be created from the class." in singleton.get(constants.DICT_KEY_PATTERN_STORY)))
-        self.assertEqual(True, ("Brick Lane Graffiti Usain Bolt" in singleton.get(constants.DICT_KEY_PATTERN_IMAGE)))        
-        self.assertEqual("/src/main/java/com/hundredwordsgof/singleton", singleton.get(constants.DICT_KEY_PATTERN_SOURCE_CODE_PACKAGE_NAME))
-        self.assertEqual("/src/test/java/com/hundredwordsgof/singleton", singleton.get(constants.DICT_KEY_PATTERN_TEST_SOURCE_CODE_PACKAGE_NAME))
-        
+        self.assertEqual("Singleton", singletonContent.get(constants.DICT_KEY_PATTERN_ID))        
+        self.assertEqual("Singleton", singletonContent.get(constants.DICT_KEY_PATTERN_NAME))
+        self.assertEqual("singleton.png", singletonContent.get(constants.DICT_KEY_PATTERN_UML_FILE_NAME))        
+        self.assertEqual(True, ("Singleton ensures that only one(single) object can be created from the class." in singletonContent.get(constants.DICT_KEY_PATTERN_STORY)))
+        self.assertEqual(True, ("Brick Lane Graffiti Usain Bolt" in singletonContent.get(constants.DICT_KEY_PATTERN_IMAGE)))        
+        self.assertEqual("/src/main/java/com/hundredwordsgof/singleton", singletonContent.get(constants.DICT_KEY_PATTERN_SOURCE_CODE_PACKAGE_NAME))
+        self.assertEqual("/src/test/java/com/hundredwordsgof/singleton", singletonContent.get(constants.DICT_KEY_PATTERN_TEST_SOURCE_CODE_PACKAGE_NAME))
+
+        templateMethodContent = arrayList[21]
+
+        # check TemplateMethod
+        self.assertEqual("TemplateMethod", templateMethodContent.get(constants.DICT_KEY_PATTERN_ID))        
+        self.assertEqual("Template Method", templateMethodContent.get(constants.DICT_KEY_PATTERN_NAME))
+        self.assertEqual("templatemethod.png", templateMethodContent.get(constants.DICT_KEY_PATTERN_UML_FILE_NAME))            
+        self.assertEqual(True, ("Defines a skeleton of an algorithm in an operation." in templateMethodContent.get(constants.DICT_KEY_PATTERN_STORY)))
+        #image content is not define, dict.get(...) for key which does not exists returns None
+        self.assertEqual(True, (templateMethodContent.get(constants.DICT_KEY_PATTERN_IMAGE) == None))        
+        self.assertEqual("/src/main/java/com/hundredwordsgof/templatemethod", templateMethodContent.get(constants.DICT_KEY_PATTERN_SOURCE_CODE_PACKAGE_NAME))
+        self.assertEqual("/src/test/java/com/hundredwordsgof/templatemethod", templateMethodContent.get(constants.DICT_KEY_PATTERN_TEST_SOURCE_CODE_PACKAGE_NAME))
         
         
 if __name__ == '__main__':
