c7d39896d2c8a98f14bb975994c3ebf709d2522e
dstar55 <dstar55@yahoo.com>
2018-09-10 21:42:36 +0200

    unit test fixed

--- src/test/java/com/hundredwordsgof/bridge/BridgeTest.java
+++ src/test/java/com/hundredwordsgof/bridge/BridgeTest.java
@@ -22,7 +22,7 @@ public class BridgeTest {
     RefinedAbstraction refinedAbstractionB = new RefinedAbstraction(
         new ConcreteImplementorB());
     // invokes operation
-    assertEquals("xxxcom.hundredwordsgof.bridge.ConcreteImplementorB",
+    assertEquals("com.hundredwordsgof.bridge.ConcreteImplementorB",
         refinedAbstractionB.operation());
   }
 }
